84cc3b2ae354029d24c95c762aeed49547f9c860
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2022-06-18 22:14:24 +0900

    矢印キーで動くように

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
 	_ "image/png"
 	"log"
+	"strings"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 var img *ebiten.Image
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+)
 
 func init() {
 	var err error
@@ -16,26 +24,54 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
 
-type Game struct{}
+type Game struct {
+	x    float64
+	y    float64
+	keys []ebiten.Key
+}
 
 func (g *Game) Update() error {
+	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
+	keyStrs := []string{}
+	for _, p := range g.keys {
+		if p.String() != "" {
+			keyStrs = append(keyStrs, p.String())
+		}
+	}
+	switch strings.Join(keyStrs, ", ") {
+	case ebiten.KeyArrowRight.String():
+		g.x += float64(2)
+	case ebiten.KeyArrowLeft.String():
+		g.x -= float64(2)
+	case ebiten.KeyArrowUp.String():
+		g.y -= float64(2)
+	case ebiten.KeyArrowDown.String():
+		g.y += float64(2)
+	default:
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.DrawImage(img, nil)
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(g.x, g.y)
+	op.GeoM.Translate(screenWidth/2, screenHeight/2)
+	screen.DrawImage(img, op)
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("ebi position\n(%0.2f, %0.2f)", g.x, g.y))
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 640, 480
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("render an image!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	g := &Game{x: 0.0, y: 0.0}
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("moving ebi!")
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
